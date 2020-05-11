6f9a6c3177e352f9efbd3e670f122c4925f14e8e
ingo <ingo@pa58.de>
2020-05-11 17:15:08 +0200

    db super class

---
+++ fbb/sudrem/db/DBConnection.java
@@ -0,0 +1,44 @@
+package fbb.sudrem.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+/**
+ * Holds the credis for connection to DB and the connection
+ * @author id
+ *
+ */
+public class DBConnection {
+
+	public Connection conn = null;
+	
+	public DBConnection() { }
+
+	public String dbuser = null;
+	public String dbpass = null;
+	public String dbhost = null;
+	public String dbname = null;
+
+	public int dbport = 3306;
+	
+	public void open() throws SQLException {
+		
+		MysqlDataSource dataSource = new MysqlDataSource();
+		dataSource.setUser(dbuser);
+		dataSource.setPassword(dbpass);
+		dataSource.setServerName(dbhost);
+		dataSource.setDatabaseName(dbname);
+		dataSource.setPort(dbport);
+		
+		conn = dataSource.getConnection();
+	}
+	
+
+	public void close() throws SQLException {
+		if ( conn != null )
+			if ( ! conn.isClosed() )
+				conn.close();
+	}
+}
