a5eb7b0dc00e3d65dd84994b1e22980167740199
ingo <ingo@pa58.de>
2020-05-11 15:04:26 +0200

    create tbl sql

--- fbb/sudrem/db/sudremdb.sql
+++ fbb/sudrem/db/sudremdb.sql
@@ -2,4 +2,10 @@
 
 -- DB sudrem
 
+DROP TABLE IF EXISTS tempera;
 
+CREATE TABLE tempera ( 
+	id int AUTO_INCREMENT PRIMARY KEY, 
+	stadt char(32) not null, 
+	temperatur int default 0, 
+	datum timestamp default current_timestamp);
