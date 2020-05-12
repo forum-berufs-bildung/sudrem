2d027ed24850d7951a984401ce760fde8cee1d81
id <ingo@pa58.de>
2020-05-12 17:33:16 +0200

    add comments to paintclass

--- fbb/sudrem/app/PaintClass.java
+++ fbb/sudrem/app/PaintClass.java
@@ -10,6 +10,12 @@ import javax.swing.JPanel;
 import fbb.sudrem.db.SudremDB;
 import fbb.sudrem.db.WetterItem;
 
+
+/**
+ * 
+ * JPanel zur daten visualisierung
+ *
+ */
 public class PaintClass extends JPanel {
 	SudremDB db;
     public PaintClass() throws SQLException{
@@ -24,14 +30,16 @@ public class PaintClass extends JPanel {
 			
 			list = db.select("BLN");
 			for (int i = 0; i < list.size() -1; i++) {
+				
 				WetterItem w = list.get(i);
 				WetterItem w2 = list.get(i + 1);
 
 				if(w.temp < 15) {
 					g.setColor(Color.BLUE);
-				}else {
+				} else {
 					g.setColor(Color.RED);
 				}
+				
 				g.fillRect(10 * i, 100 - (w.temp * 2), 3, 3);
 				g.drawLine(10 * i, 100 - (w.temp * 2), 10 * i +10, 100 - (w2.temp * 2));
 				System.out.println(w.toString());
@@ -42,8 +50,6 @@ public class PaintClass extends JPanel {
 			e.printStackTrace();
 		}
 		
-
-
     }
     
     public static void main(String[] args){
